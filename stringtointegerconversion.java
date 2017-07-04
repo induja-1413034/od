fe8eefddee58dcb9a34d1c4b9e24d282e717b176
induja-1413034 <induja26.1997@gmail.com>
2017-07-04 15:33:33 +0530

    Add files via upload

---
+++ stringtointegerconversion.java
@@ -0,0 +1,14 @@
+package rou;
+
+import java.util.Scanner;
+
+public class stringtointegerconversion {
+public static void main(String[] args){
+	String a;
+	Scanner s=new Scanner(System.in);
+	a=s.next();
+	int b=Integer.parseInt(a);
+	System.out.println(b);
+	
+}
+}
