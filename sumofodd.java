dd036eb2c521428e0a6362bd366b0488c6a05dba
induja-1413034 <induja26.1997@gmail.com>
2017-07-05 10:57:14 +0530

    Add files via upload

---
+++ sumofodd.java
@@ -0,0 +1,12 @@
+package rou;
+public class sumofodd {
+public static void main(String[] args){
+int sum=0;
+for(int i=0;i<=20;i++){
+	if((i%2)!=0){
+		sum=sum+i;
+	}
+}
+System.out.println(sum);
+}
+}
