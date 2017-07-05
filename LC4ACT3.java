3b0d87246fb4c4157690d1d17836b0384e556ce5
induja-1413034 <induja26.1997@gmail.com>
2017-07-05 09:27:15 +0530

    Add files via upload

---
+++ LC4ACT3.java
@@ -0,0 +1,12 @@
+package LC4;
+import java.util.Scanner;
+public class LC4ACT3 {
+public static void main(String[] args){
+	String a;
+	String b;
+	Scanner s=new Scanner(System.in);
+	a=s.next();
+	b=s.next();
+	System.out.println(a+" "+b);
+}
+}
