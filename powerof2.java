92d6a3c2d5aae58f0e09c2d53c482911d3a1349a
induja-1413034 <induja26.1997@gmail.com>
2017-07-14 12:24:55 +0530

    Add files via upload

---
+++ powerof2.java
@@ -0,0 +1,12 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class powerof2 {
+public static void main(String[] args){
+	int a;
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	System.out.println(Math.pow(a, 2));
+}
+}
