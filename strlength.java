8121aa3746a9d85accb43a6ce17a0615f9eae3fa
induja-1413034 <induja26.1997@gmail.com>
2017-07-19 20:48:36 +0530

    Add files via upload

---
+++ strlength.java
@@ -0,0 +1,22 @@
+package guviprgms;
+
+import java.util.Scanner;
+
+public class strlength {
+public static void main(String[] args){
+	String a;
+	String b;
+	Scanner s=new Scanner(System.in);
+	a=s.next();
+	b=s.next();
+	int c=a.length();
+	int d=b.length();
+	int e=a.length()-b.length();
+	if(c==d){
+		System.out.println(a+b);	
+	}
+	else if(c>d){
+		System.out.println(a.substring(e));
+	}
+}
+}
