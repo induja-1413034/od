5a9d58764e468abe8f21dc9c396a0f810422fb1d
induja-1413034 <induja26.1997@gmail.com>
2017-07-22 13:52:29 +0530

    Add files via upload

---
+++ uniquechar.java
@@ -0,0 +1,26 @@
+package LogicPrograms;
+
+import java.util.Scanner;
+
+public class uniquechar {
+public static void main(String[] args){
+	String a;
+	Scanner s=new Scanner(System.in);
+	a=s.nextLine();
+	char[] b=a.toCharArray();
+	for(int i=0;i<b.length;i++){
+		for(int j=i+1;j<b.length;j++){
+			if(a.charAt(i)==a.charAt(j)){
+				b[i]='\0';
+				b[j]='\0';//duplicate elements wont display
+			}
+		}
+	}
+	for(int i=0;i<b.length;i++){
+		if(b[i]!='\0'){
+	System.out.print(b[i]);
+		}
+}
+	//System.out.println(count);
+}
+}
